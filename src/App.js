74e1c7e839cffa111e1730f6f6a88ca17fc5c12b
Shovan Banik <53087831+Shovan-Banik@users.noreply.github.com>
2023-03-21 14:52:31 +0600

    fetch data and show data using react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
       <Counter></Counter>
+      <ExternalUsers></ExternalUsers>
     </div>
   );
 }
+function ExternalUsers(){
+  const [users, setUsers]=useState([]);
+  useEffect( ()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res=>res.json())
+    .then(data=>setUsers(data));
+  },[]);
+  return (
+    <div>
+      <h2>External Users: </h2>
+      <p>{users.length}</p>
+      {
+      users.map(user=><User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+function User(props){
+  return (
+    <div style={{border: '4px solid blue', margin: '10px', backgroundColor: 'black', color:' white', borderRadius: '10px'}}>
+      <h2>Name: {props.name}</h2>
+      <p>Email: {props.email}</p>
+    </div>
+  )
+}
+
+
 function Counter(){
   const [count, setCount]=useState(0);
   const increaseCount=()=>setCount(count + 1);

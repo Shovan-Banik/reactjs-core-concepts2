ca127e57c6dec183ffcb5806b7b0553c38dad9f8
Shovan Banik <53087831+Shovan-Banik@users.noreply.github.com>
2023-03-21 13:57:21 +0600

    counter project by react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter></Counter>
     </div>
   );
 }
+function Counter(){
+  const [count, setCount]=useState(0);
+  const increaseCount=()=>setCount(count + 1);
+  const decreaseCount=()=>setCount(count-1);
+  // multiline function for count
+  // const increaseCount=()=>{
+  //   const newCount=count + 1;
+  //   setCount(newCount);
+  // }
+  return (
+    <div>
+      <h1>count: {count}</h1>
+      <button onClick={increaseCount}>increase</button>
+      <button onClick={decreaseCount}>decrease</button>
+    </div>
+  )
+}
+
 
 export default App;
+
+
+// function App() {
+//   const products=[
+//     {name: 'laptop', price: 57000},
+//     {name: 'phone', price: 19500},
+//     {name: 'watch', price: 1500},
+//     {name: 'headphone', price: 350}
+//   ];
+//   return (
+//     <div className="App">
+//       {
+//         products.map(product=><Product name={product.name} price={product.price}></Product>)
+//       }
+
+//       {/* <Product name='leptop' price='20000'></Product> */}
+//     </div>
+//   );
+// }
+// function Product(props){
+//   console.log(props);
+//   return (
+//     <div className='product'>
+//       <h3>Name: {props.name}</h3>
+//       <p>Price: {props.price}</p>
+//     </div>
+//   )
+// }
